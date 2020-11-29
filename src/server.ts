f50fae136e6f303b27ee301a5cb92d768b7874df
Murilo G. de Oliveira <murilo_879@hotmail.com>
2020-11-29 14:45:56 -0400

    Add TS configuration and paths

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello Word'});
-})
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Word" });
+});
 
-app.listen('3333'); 
+app.listen("3333");
